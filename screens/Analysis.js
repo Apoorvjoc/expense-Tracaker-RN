6776a7109b5de3bb9b3d721a273597c844db2dc1
Apoorv <hlwapoorv@gmail.com>
2021-12-22 16:51:45 +0530

    main before creating new version4

---
+++ screens/Analysis.js
@@ -0,0 +1,62 @@
+import { PieChart } from "react-native-chart-kit";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+
+const data = [
+  {
+    name: "Income",
+    amount: 5000,
+    color: "rgba(131, 167, 234, 1)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15,
+  },
+  {
+    name: "Expense",
+    amount: 2800,
+    color: "#F00",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 15,
+  },
+];
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
+};
+
+const Analysis = () => {
+  return (
+    <View style={styles.container}>
+      <PieChart
+        data={data}
+        width={screenWidth}
+        height={250}
+        chartConfig={chartConfig}
+        accessor={"amount"}
+        backgroundColor={"transparent"}
+        paddingLeft={"15"}
+        center={[5, 20]}
+        absolute
+      />
+    </View>
+  );
+};
+
+export default Analysis;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignContent: "center",
+    flex: 1,
+  },
+});
